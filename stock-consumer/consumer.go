6baad6d2eee8620d3b579d893ef11bb1a9efefa6
Mando-Rian <pluess87@gmail.com>
2024-06-15 10:37:36 +0200

    Ping zu MongoDB hinzugefügt. Umgebungsvariable "COMPANYNAME" hinzugefügt.

--- stock-consumer/consumer.go
+++ stock-consumer/consumer.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"log"
 
-	amqp "github.com/rabbitmq/amqp091-go"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// failOnError is a helper function to log error messages.
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
 }
 
+// aggregateStock avarages the price from a batch sise and inputs it into the MongoDB:
 func aggregateStock(msgsBody json) float64 {
 
 	var sum float64 = 0
@@ -33,11 +37,18 @@ func aggregateStock(msgsBody json) float64 {
 
 func main() {
 
+	// Connecting to MongoDB.
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=rs0"))
 	failOnError(err, "Failed to connect to MongoDB")
 
-	usersCollection := client.Database("Stock").Collection(companyName)
+	// Test connection with ping
+	err = client.Ping(context.TODO(), readpref.Primary())
+	failOnError(err, "Failed to ping MongoDB")
 
+	// Creating Database and Collection.
+	usersCollection := client.Database("Market").Collection("Stocks")
+
+	// Connecting to RabbitMQ.
 	conn, err := amqp.Dial("amqp://stockmarket:supersecret123@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -48,7 +59,7 @@ func main() {
 
 	// Declaring the variable for the queue name, which is the same
 	// as the stock company.
-	var companyName string = "MSFT"
+	var companyName string = os.Getenv("COMPANYNAME")
 
 	q, err := ch.QueueDeclare(
 		companyName, // name of the queue is the same as the stock
